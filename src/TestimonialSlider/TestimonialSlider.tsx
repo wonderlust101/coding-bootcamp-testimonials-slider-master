c479df608054271f72aab7394379e9535572a273
Sergei Borja <sergei.borja0107@gmail.com>
2024-09-13 17:04:31 -0600

    Finished Project.

--- src/TestimonialSlider/TestimonialSlider.tsx
+++ src/TestimonialSlider/TestimonialSlider.tsx
@@ -15,51 +15,43 @@ interface TestimonialSliderProps {
 }
 
 export default function TestimonialSlider({userData}: TestimonialSliderProps) {
-    const [nextTest, setNextTest] = useState(userData.length - 1);
     const [displayedTest, setDisplayedTest] = useState(0);
-    const [previousTest, setPreviousTest] = useState(1);
 
     function handleNextTest() {
-        setPreviousTest(displayedTest);
-        setDisplayedTest(nextTest);
-        if (nextTest + 1 === userData.length) {
-            setNextTest(0);
+        if (displayedTest + 1 === userData.length) {
+            setDisplayedTest(0);
             return;
         }
-        setNextTest(nextTest + 1);
+        setDisplayedTest(displayedTest + 1);
     }
 
     function handlePreviousTest() {
-        setNextTest(displayedTest);
-        setDisplayedTest(previousTest);
-        if (previousTest - 1 < 0) {
-            setPreviousTest(userData.length - 1);
+        if (displayedTest - 1 < 0) {
+            setDisplayedTest(userData.length - 1);
             return;
         }
-        setPreviousTest(previousTest - 1);
+        setDisplayedTest(displayedTest - 1);
     }
 
     return (
-        <div className='testimonials'>
-            <div className='testimonials__image-slider'>
+        <div className="testimonials">
+            <div className="testimonials__image-slider">
                 <div className="testimonials__image-container">
-                    <TestimonialImage userData={userData[previousTest]}/>
                     <TestimonialImage userData={userData[displayedTest]}/>
-                    <TestimonialImage userData={userData[nextTest]}/>
                 </div>
 
                 <div className="testimonials__buttons">
-                    <TestimonialButtons onClick={handlePreviousTest} buttonType='previous'/>
-                    <TestimonialButtons onClick={handleNextTest} buttonType='next'/>
+                    <TestimonialButtons onClick={handlePreviousTest} buttonType="previous"/>
+                    <TestimonialButtons onClick={handleNextTest} buttonType="next"/>
                 </div>
             </div>
 
-            <div className='testimonials__text'>
+            <div className="testimonials__text">
                 <p className="testimonials__quote">{userData[displayedTest].quote}</p>
 
-                <div className='testimonials__user-test'>
-                    <p className='testimonials__user-name'>{userData[displayedTest].firstName + ' ' + userData[displayedTest].lastName}</p>
-                    <p className='testimonials__user-role'>{userData[displayedTest].role}</p>
+                <div className="testimonials__user-test">
+                    <p className="testimonials__user-name">{userData[displayedTest].firstName + ' ' + userData[displayedTest].lastName}</p>
+                    <p className="testimonials__user-role">{userData[displayedTest].role}</p>
                 </div>
             </div>
         </div>
